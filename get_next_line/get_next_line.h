f784b3ac1a5cdecd17e942c07b9348ba23482c8d
Iker Del Pozo De Frutos <idel-poz@c1r7s3.42urduliz.com>
2024-04-06 16:49:00 +0200

    fix normi

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: idel-poz <idel-poz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:56:58 by idel-poz          #+#    #+#             */
-/*   Updated: 2024/04/06 14:18:30 by idel-poz         ###   ########.fr       */
+/*   Updated: 2024/04/06 16:34:53 by idel-poz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ typedef struct s_stack
 {
 	char			*buf;
 	struct s_stack	*next;
-}	str_stack;
+}	t_str_stack;
 
 char		*get_next_line(int fd);
 
-void		stack_next_line(int fd, str_stack **stack);
-int			str_has_line_break(str_stack *stack);
-void		append_to_stack(str_stack **stack, char *buf);
-int			get_line_len(str_stack *stack);
-void		copy_str_line(str_stack *stack, char *line);
-char		*read_line(str_stack *stack);
-void		clean_stack(str_stack **stack);
-str_stack	*get_last_stack(str_stack *stack);
-char		*get_remaining_buffer(str_stack **stack);
+void		stack_next_line(int fd, t_str_stack **stack);
+int			str_has_line_break(t_str_stack *stack);
+void		append_to_stack(t_str_stack **stack, char *buf);
+int			get_line_len(t_str_stack *stack);
+void		copy_str_line(t_str_stack *stack, char *line);
+char		*read_line(t_str_stack *stack);
+void		clean_stack(t_str_stack **stack);
+t_str_stack	*get_last_stack(t_str_stack *stack);
+char		*get_remaining_buffer(t_str_stack **stack);
 int			str_len(char *str);
 
 #endif
