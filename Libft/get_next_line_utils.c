7cc6a5629841d56b861bf25c7579e4094f07f762
Iker del Pozo <ikerdelpozo@gmail.com>
2024-09-21 12:57:56 +0200

    fix libft normi

--- Libft/get_next_line_utils.c
+++ Libft/get_next_line_utils.c
@@ -6,36 +6,15 @@
 /*   By: idel-poz <idel-poz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:56:49 by idel-poz          #+#    #+#             */
-/*   Updated: 2024/08/20 22:50:06 by idel-poz         ###   ########.fr       */
+/*   Updated: 2024/09/21 12:57:12 by idel-poz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	stack_next_line(int fd, str_stack **stack)
+int	str_has_line_break(t_stack *stack)
 {
-	int		char_read;
-	char	*buf;
-
-	while (!str_has_line_break(*stack))
-	{
-		buf = malloc(BUFFER_SIZE + 1);
-		if (!buf)
-			return;
-		char_read = read(fd, buf, BUFFER_SIZE);
-		if (char_read <= 0)
-		{
-			free(buf);
-			return;
-		}
-		buf[char_read] = 0;
-		append_to_stack(stack, buf);
-	}
-}
-
-int	str_has_line_break(str_stack *stack)
-{
-	int i;
+	int	i;
 
 	if (!stack)
 		return (0);
@@ -53,24 +32,7 @@ int	str_has_line_break(str_stack *stack)
 	return (0);
 }
 
-void	append_to_stack(str_stack **stack, char *buf)
-{
-	str_stack	*new;
-	str_stack	*last;
-
-	new = malloc(sizeof(str_stack));
-	if (!new)
-		return;
-	last = get_last_stack(*stack);
-	if (!last)
-		*stack = new;
-	else
-		last->next = new;
-	new->buf = buf;
-	new->next = NULL;
-}
-
-int	get_line_len(str_stack *stack)
+int	get_line_len(t_stack *stack)
 {
 	int	len;
 	int	i;
@@ -91,7 +53,7 @@ int	get_line_len(str_stack *stack)
 	return (len);
 }
 
-char	*copy_str_line(str_stack *stack, char *line)
+char	*copy_str_line(t_stack *stack, char *line)
 {
 	int		i;
 	int		j;
@@ -112,45 +74,7 @@ char	*copy_str_line(str_stack *stack, char *line)
 	return (line);
 }
 
-char	*read_line(str_stack *stack)
-{
-	int		line_len;
-	char	*line;
-
-	line_len = get_line_len(stack);
-	if (line_len == 0)
-		return (NULL);
-	line = malloc(line_len + 1);
-	if (!line)
-		return (NULL);
-	copy_str_line(stack, line);
-	return (line);
-}
-
-void	clean_stack(str_stack **stack)
-{
-	str_stack	*tmp;
-	char		*buf;
-
-	buf = get_remaining_buffer(stack);
-	while (*stack)
-	{
-		tmp = (*stack)->next;
-		free((*stack)->buf);
-		free(*stack);
-		*stack = tmp;
-	}
-	free(*stack);
-	if (!buf || !buf[0])
-		return;
-	*stack = malloc(sizeof(str_stack));
-	if (!*stack)
-		return;
-	(*stack)->buf = buf;
-	(*stack)->next = NULL;
-}
-
-str_stack	*get_last_stack(str_stack *stack)
+t_stack	*get_last_stack(t_stack *stack)
 {
 	if (!stack)
 		return (NULL);
@@ -159,12 +83,12 @@ str_stack	*get_last_stack(str_stack *stack)
 	return (stack);
 }
 
-char	*get_remaining_buffer(str_stack **stack)
+char	*get_remaining_buffer(t_stack **stack)
 {
-	str_stack	*last;
-	char		*buf;
-	int			i;
-	int			j;
+	t_stack	*last;
+	char	*buf;
+	int		i;
+	int		j;
 
 	last = get_last_stack(*stack);
 	buf = malloc(BUFFER_SIZE + 1);
@@ -177,20 +101,10 @@ char	*get_remaining_buffer(str_stack **stack)
 	while (last->buf[i] && last->buf[++i])
 		buf[j++] = last->buf[i];
 	buf[j] = 0;
-	if (str_len(buf) == 0)
+	if (ft_strlen(buf) == 0)
 	{
 		free(buf);
 		return (NULL);
 	}
 	return (buf);
-}
-
-int	str_len(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
